4eb9abb4276621b2625c28b980ec00d3c3b79c2e
win <wwinnn478@gmail.com>
2023-12-26 11:41:22 +0700

    lab11_1.cpp

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,43 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main() {
+    cout << "Press Enter 3 times to reveal your future.\n";
+    for (int i = 0; i < 3; ++i) {
+        while (cin.get() != '\n');
+    }
+
+    srand(time(0));
+    int grade = rand() % 8; 
+
+    switch (grade) {
+        case 0:
+            cout << "You will get A in this 261102.";
+            break;
+        case 1:
+            cout << "You will get B+ in this 261102.";
+            break;
+        case 2:
+            cout << "You will get B in this 261102.";
+            break;
+        case 3:
+            cout << "You will get C+ in this 261102.";
+            break;
+        case 4:
+            cout << "You will get C in this 261102.";
+            break;
+        case 5:
+            cout << "You will get D+ in this 261102.";
+            break;
+        case 6:
+            cout << "You will get D in this 261102.";
+            break;
+        case 7:
+            cout << "You will get F in this 261102.";
+            break;
+    }
+
+    return 0;
+}
